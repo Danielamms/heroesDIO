79e78d5f62ee07a35487136accafa5456f12958c
daniela.machado <daniela.machado@tacom.com.br>
2021-03-08 16:18:36 -0300

    outro comite

--- src/main/java/com/digital/heroesapi/HeroesapiApplication.java
+++ src/main/java/com/digital/heroesapi/HeroesapiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableDynamoDBRepositories
 @SpringBootApplication
 public class HeroesapiApplication {
-
+//para fazer subirt o projeto
 	public static void main(String[] args) {
 		SpringApplication.run(HeroesapiApplication.class, args);
 		   System.out.println("super poderes com webflux");
